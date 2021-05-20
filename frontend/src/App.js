840f1de5d7281a2daeed7e8c6ddb85dae3313147
RahulVirahaniya <53095409+RahulVirahaniya@users.noreply.github.com>
2021-05-20 17:33:26 +0530

    add cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
-
+import CartScreen from './screens/CartScreen';
 function App() {
   return (
     <Router>
@@ -18,6 +18,7 @@ function App() {
 
           {/* to load other screen like product and :id means anything after /product is :id */}
           <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
 
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,31 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
 import Product from '../components/Product';
-import axios from 'axios';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { listProducts } from '../actions/productActions';
+
 // import products from '../products';
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
 
   // use effect gets loaded the mommet the component gets loaded so call api here
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    };
+  // useEffect(() => {
+  //   const fetchProducts = async () => {
+  //     const { data } = await axios.get('/api/products');
+  //     setProducts(data);
+  //   };
 
-    fetchProducts();
-  }, []);
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
       <h1>latest Products</h1>
-      <Row>
+      {/* <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} lg={4} xl={3}>
             <Product product={product} />
           </Col>
         ))}
-      </Row>
+      </Row> */}
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
